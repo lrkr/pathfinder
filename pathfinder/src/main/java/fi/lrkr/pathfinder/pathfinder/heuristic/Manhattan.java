b90448f135f4702a53350a86c993152435cdc9fa
lrkr <lkarvonen@gmail.com>
2017-08-22 19:11:54 +0300

    heuristics fixes

--- pathfinder/src/main/java/fi/lrkr/pathfinder/pathfinder/heuristic/Manhattan.java
+++ pathfinder/src/main/java/fi/lrkr/pathfinder/pathfinder/heuristic/Manhattan.java
@@ -9,6 +9,8 @@ public class Manhattan implements Heuristic {
     public double calculateHScore(Vertex v1, Vertex v2, Vertex v3) {
         Location l1 = v1.getLocation();
         Location l2 = v2.getLocation();
-        return Math.abs(l1.getX() - l2.getX()) + Math.abs(l1.getY() - l2.getY());
+        double dx = l1.getX() - l2.getX();
+        double dy = l1.getY() - l2.getY();
+        return Math.abs(dx) + Math.abs(dy);
     }
 }
